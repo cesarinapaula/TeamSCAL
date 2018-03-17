d791a10c6c0847619c574e8374d9d3602221ddbd
cesarinapaula <cesarinapaula@ac.c4q.nyc>
2018-03-17 10:57:21 -0400

    Uploaded Folder with Mock File

---
+++ CapstoneProject/client/src/components/Polls/mock.js
@@ -0,0 +1,15 @@
+//mock file to upload folders
+
+import React from 'react';
+
+class Mock extends React.Component {
+    render(){
+        return (
+            <div>
+                <h1/>::headnod::</h1>
+            </div>
+        )
+    }
+}
+
+export default Mock;
