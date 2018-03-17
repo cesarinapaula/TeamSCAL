cc1cf15a1848446b935c81dcfe392d0e7c889552
cesarinapaula <cesarinapaula@ac.c4q.nyc>
2018-03-17 00:36:00 -0400

    Directory Upload

---
+++ CapstoneProject/client/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>The future home of Planz</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
