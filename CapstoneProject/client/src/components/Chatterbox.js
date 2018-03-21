b1836cfdcad217661b42f5eee30bc451a592af08
LeShanda <shandamiller91@gmail.com>
2018-03-21 02:48:07 -0400

    chatterbox/wall

--- CapstoneProject/client/src/components/Chatterbox.js
+++ CapstoneProject/client/src/components/Chatterbox.js
@@ -1,10 +1,75 @@
 import React from "react";
+import ReactDom from "react-dom";
+import "../index.css";
 
-const Chatterbox = () => (
-  <div>
-    <h2> Planz Logo</h2>
-<button> Login</button>   
-  </div>
-);
+
+const generateId = () => {
+    return Math.random()
+      .toString(34)
+      .slice(2);
+  };
+  const ChatterWall = ({ handleSubmit, inputText, handleInputText }) => (
+    <div>
+      <h2>Issa Wall </h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={inputText} onChange={handleInputText} />
+
+        <button type="submit"> Post </button>
+      </form>
+    </div>
+  );
+  const Comment = ({ comment }) => <li className='comment'>{comment.text}</li>;
+
+  const ChatterList = ({ comments }) => (
+    <ul>{comments.map(comment => <Comment comment={comment} />)}</ul>
+  );
+  
+
+class Chatterbox extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            inputText:"",  
+                       
+            
+            name:"",
+            comments:[]            
+            
+        };
+    }
+    addComment = e => {
+        e.preventDefault();
+        const { comments, inputText,name } = this.state;
+        const newComment = { text: inputText, id: generateId() };
+        const newComments = [newComment,...comments ];
+        
+        this.setState({
+            inputText:"",
+            comments: newComments
+        });
+    };
+    handleInputText = e => {
+        this.setState({
+          inputText: e.target.value
+        });
+      };
+      render(){
+        const { inputText, comments } = this.state;
+        return(
+            <div className='chatterbox'>
+                <div className='ChatterWall'>
+                <ChatterWall 
+                handleSubmit={this.addComment}
+                inputText={inputText}
+                handleInputText={this.handleInputText}
+                />
+                </div>
+                <div className='chatterlist'>
+                <ChatterList comments={comments} />
+                </div>
+            </div>
+        )
+      }
+}
 
 export default Chatterbox;
--- CapstoneProject/client/src/components/Planz.js
+++ CapstoneProject/client/src/components/Planz.js
@@ -1,17 +1,20 @@
 import React from "react";
+import Chatterbox from "./Chatterbox";
 
 const Planz = () => (
   <div>
     <h2> Planz Logo</h2>
 
     <p>Occasian Goes Here</p>
+    import ReactDom from "react-dom";
 
     <p>Location:TBA</p>
     <p>Time:TBA</p>
     <p>DAte:TBA</p>
     {"  "} 
 
-<h2>Chatter Box</h2>
+
+<Chatterbox />
 <h2>Decide on a Plan(polls)</h2>
   </div>
 );
