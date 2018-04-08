a977f2e94a86b09968fa2d8cc6e93f05d0de2643
cesarinapaula <cesarinapaula@ac.c4q.nyc>
2018-04-08 18:54:07 -0400

    fix poll

--- CapstoneProject/client/src/components/Polls/PollRenderingTime.js
+++ CapstoneProject/client/src/components/Polls/PollRenderingTime.js
@@ -13,7 +13,7 @@ export default class RenderTimeDatePoll extends React.Component{
                 <label><input type='radio' value={choiceOne} onChange={this.handleSelect}/>{choiceOne}</label><br/>
                 <label><input type='radio' value={choiceTwo} onChange={this.handleSelect}/>{choiceTwo}</label><br/>
                 <label id={hiddenOrAppear3}><input type='radio' value={choiceThree} onChange={handleSelect}/>{choiceThree}</label><br/>
-                <label id={hiddenOrAppear4}><input type='radio' id={hiddenOrAppear3} value={choiceFour} onChange={handleSelect}/>{choiceFour}</label><br/>
+                <label id={hiddenOrAppear4}><input type='radio' value={choiceFour} onChange={handleSelect}/>{choiceFour}</label><br/>
                 <label id={hiddenOrAppear5}><input type='radio' value={choiceFive} onChange={handleSelect}/>{choiceFive}</label><br/>
                 <label id={hiddenOrAppear6}><input type='radio' value={choiceSix} onChange={handleSelect}/>{choiceSix}</label><br/>
                 <label id={hiddenOrAppear7}><input type='radio' value={choiceSeven} onChange={handleSelect}/>{choiceSeven}</label><br/>
