e575ce0abf1fcee7f2cd7cdeac9f1f7dcdea4651
cesarinapaula <cesarinapaula@ac.c4q.nyc>
2018-04-09 22:04:15 -0400

    Poll stuff

--- CapstoneProject/client/src/components/Polls/PollRenderingLocation.js
+++ CapstoneProject/client/src/components/Polls/PollRenderingLocation.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import Timer from "./Timer";
 import '../../index.css';
+import { Button } from 'semantic-ui-react';
 
 
 
 export default class RenderLocationPoll extends React.Component{
     render(){
-        const { pollStyle, timerStyle, choiceOne, choiceTwo, choiceThree, choiceFour, choiceFive, handleSelect, handleVote, submitVote, hiddenOrAppear3, hiddenOrAppear4, hiddenOrAppear5} = this.props;
+        const { styling, choiceOne, choiceTwo, choiceThree, choiceFour, choiceFive, handleSelect, handleVote, submitVote, hiddenOrAppear3, hiddenOrAppear4, hiddenOrAppear5} = this.props;
     
         return(
-        <div>
-            <div id={pollStyle}>
-                <p>Vote for where you want to go</p>
-                <label><input type='radio' value={choiceOne} onChange={handleSelect}/>{choiceOne}</label><br/>
-                <label><input type='radio' value={choiceTwo} onChange={handleSelect}/>{choiceTwo}</label><br/>
-                <label id={hiddenOrAppear3}><input type='radio' value={choiceThree} onChange={handleSelect}/>{choiceThree}</label><br/>
-                <label id={hiddenOrAppear4}><input type='radio' value={choiceFour} onChange={handleSelect}/>{choiceFour}</label><br/>
-                <label id={hiddenOrAppear5}><input type='radio' value={choiceFive} onChange={handleSelect}/>{choiceFive}</label><br/>
-                <button onClick={handleVote}>Vote</button>
-                <button onClick={submitVote}>Final Answer?</button>
-            </div>
+        <div className={styling}>
+            <h3>Vote for where you want to go!</h3>
+                <label><input type='radio' value={choiceOne} name='location' onChange={handleSelect}/>{choiceOne}</label><br/>
+                <label><input type='radio' value={choiceTwo} name='location' onChange={handleSelect}/>{choiceTwo}</label><br/>
+                <label id={hiddenOrAppear3}><input type='radio' value={choiceThree} name='location' onChange={handleSelect}/>{choiceThree}</label><br/>
+                <label id={hiddenOrAppear4}><input type='radio' value={choiceFour} name='location' onChange={handleSelect}/>{choiceFour}</label><br/>
+                <label id={hiddenOrAppear5}><input type='radio' value={choiceFive} name='location' onChange={handleSelect}/>{choiceFive}</label><br/>
+                <Button onClick={handleVote}>Vote</Button>
+                <Button onClick={submitVote}>Final Answer?</Button>
+            
         </div>
     )
 }
