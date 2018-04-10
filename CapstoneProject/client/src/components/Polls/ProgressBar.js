82090471289321224de26922be092c7c26bd0885
cesarinapaula <cesarinapaula@ac.c4q.nyc>
2018-04-09 22:45:53 -0400

    Progress

--- CapstoneProject/client/src/components/Polls/ProgressBar.js
+++ CapstoneProject/client/src/components/Polls/ProgressBar.js
@@ -6,12 +6,7 @@ class ProgressBar extends React.Component {
     const {value, total, color } = this.props;
     return (
         <Progress progress='value' value={value} color={color} total={total} />
-
-    )
+       )
+    }
 }
-}
-<<<<<<< HEAD
-export default ProgressBar
-=======
 export default ProgressBar;
->>>>>>> e3bc7fdb145b776a38927e5ed63eb61038e4a8e6
